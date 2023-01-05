b1fa1eac56d0952bc306bcfcdb527c0c68afc51a
Sakarias <post@sakarias.no>
2023-01-05 22:10:50 +0100

    Moved Port variable to .env

--- main.go
+++ main.go
@@ -10,15 +10,14 @@ import (
 	"github.com/joho/godotenv"
 )
 
-// const PORT = os.Getenv("APP_PORT") // This is how you would do it in production
-// const PORT = "8091"
-
 func init() {
 	if err := godotenv.Load("config/env/.env"); err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
 }
 func main() {
+	PORT := os.Getenv("APP_PORT")
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -31,8 +30,8 @@ func main() {
 <p>Here you will soon be able to se my awesome resume</p>`)
 	})
 
-	fmt.Printf("Starting server on port %s", os.Getenv("APP_PORT"))
-	if err := http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("APP_PORT")), r); err != nil {
+	fmt.Printf("Starting server on port %s", PORT)
+	if err := http.ListenAndServe(fmt.Sprintf(":%s", PORT), r); err != nil {
 		panic(fmt.Sprintf("Could not start http server: %v", err))
 	}
 }
